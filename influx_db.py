775718dfbdbb3f34bd857d0403ae1fd2fb10cf74
HarryGriffiths1 <159850280+HarryGriffiths1@users.noreply.github.com>
2025-09-23 10:39:33 +0000

    added debug print statments

--- influx_db.py
+++ influx_db.py
@@ -3,6 +3,11 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 def send_data(token,org,url,bucket,weight):
+    print("function running")
+    print(token)
+    print(weight)
+    print(org)
+    print(url)
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
     write_api = client.write_api(write_options=SYNCHRONOUS)
@@ -13,4 +18,6 @@ def send_data(token,org,url,bucket,weight):
             .field("Weight", weight)
         )
 
+    print(point)
+    
     write_api.write(bucket=bucket, org=org, record=point)
