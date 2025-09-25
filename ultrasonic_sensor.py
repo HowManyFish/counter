84e3477446bd30bf8c381dc4b18e89bf981abca5
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-25 21:55:00 +1200

    debug shiz

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -7,12 +7,16 @@ def averaged_scan(sensors: list, number_scans: int) -> list[(float, list[float])
     for i in range(len(sensors)):
         output.insert(i, (0.0, list()))
 
+    print(output)
+
     for scan in range(number_scans):
         for sensor_id, sensor in enumerate(sensors):
             output[sensor_id][1].append(sensor.scan())
+    print(output)
 
     for index, sensor_scan in enumerate(output):
         output.insert(index, (mean(sensor_scan[1]), sensor_scan[1]))
+    print(output)
 
     return output
 
