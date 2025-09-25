28fd846c0e845700ddd9622a594f6bb2ae374ed6
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-25 21:30:47 +1200

    more silly

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -5,8 +5,8 @@ def averaged_scan(sensors: list, number_scans: int) -> list[(float, list[float])
     output: list[(float, list[float])] = list()
 
     for scan in range(number_scans):
-        for sensor_id in range(len(sensors) - 1):
-            output[sensor_id] = (0.0, sensors[sensor_id].scan())
+        for sensor_id, sensor in enumerate(sensors):
+            output.insert(sensor_id, (0.0, sensor.scan()))
 
     for index, sensor_scan in enumerate(output):
         output.insert(index, mean(sensor_scan[1]), sensor_scan[1])
